8eb3655b58e5e5c7200984f53c9b1d9f80780b54
Christopher Schuster <cs@livoris.de>
2014-03-27 16:14:28 -0700

    replaced AST with thunks

--- bfhs.hs
+++ bfhs.hs
@@ -5,47 +5,38 @@ import Control.Monad.Trans.State
 import Text.Parsec
 import Text.Parsec.String (Parser)
 
-data BFProg = Inc | Dec | PutC | GetC | Next | Prev | Loop [BFProg] | Skip
-
--- Brainfuck Parser
-bfParser :: Parser BFProg
-bfParser =  (noneOf "+-.,><[]" >> return Skip)
-        <|> (char '+' >> return Inc)
-        <|> (char '-' >> return Dec)
-        <|> (char '.' >> return PutC)
-        <|> (char ',' >> return GetC)
-        <|> (char '>' >> return Next)
-        <|> (char '<' >> return Prev)
-        <|> do char '['
-               s <- many1 bfParser
-               char ']'
-               return $ Loop s
-
+-- Brainfuck State
 type BFState = ([Char], Char, [Char])
 
-type BFThread = StateT BFState IO ()
+-- A Brainfuck program is simply a State Transformation Monad
+type BFThunk = StateT BFState IO ()
 
-withCurrent :: (Char -> IO Char) -> BFThread
+-- Helper function to do something with the current cell (inc. IO)
+withCurrent :: (Char -> IO Char) -> BFThunk
 withCurrent f = do (l, c, r) <- get
                    c' <- liftIO (f c)
                    put (l, c', r)
 
-bf :: BFProg -> BFThread
-bf Skip = return ()
-bf Inc = withCurrent $ return . chr . succ . ord
-bf Dec = withCurrent $ return . chr . pred . ord
-bf PutC = withCurrent $ \s -> putChar s >> return s
-bf GetC = withCurrent $ \_ -> getChar
-bf Next = get >>= \(l, c, r:rs) -> put (c:l, r, rs)
-bf Prev = get >>= \(l:ls, c, r) -> put (ls, l, c:r)
-bf (Loop body) = do (_,c,_) <- get
-                    case c of
-                      '\0' -> return ()
-                      _    -> mapM_ bf (body ++ [Loop body])
+-- Brainfuck parser and thunk generator
+bf :: Parser BFThunk
+bf =  (noneOf "+-.,><[]" >> return (return ()))
+  <|> (char '+' >> return (withCurrent $ return . chr . succ . ord))
+  <|> (char '-' >> return (withCurrent $ return . chr . pred . ord))
+  <|> (char '.' >> return (withCurrent $ \s -> putChar s >> return s))
+  <|> (char ',' >> return (withCurrent $ \_ -> getChar))
+  <|> (char '>' >> return (get >>= \(l, c, r:rs) -> put (c:l, r, rs)))
+  <|> (char '<' >> return (get >>= \(l:ls, c, r) -> put (ls, l, c:r)))
+  <|> do char '['
+         body <- many1 bf
+         char ']'
+         let loop = do (_,c,_) <- get
+                       case c of '\0' -> return ()
+                                 _    -> sequence_ (body ++ [loop])
+         return loop
 
 main :: IO ()
 main = do (file:_) <- getArgs
           code <- readFile file
-          case parse (many1 bfParser) "" code of
+          case parse (many1 bf) "" code of
             Left err -> print $ "Error while parsing: " ++ show err
-            Right prog -> evalStateT (mapM_ bf prog) ([], '\0', repeat '\0')
+            Right prog -> evalStateT (sequence_ prog) ([], '\0', repeat '\0')
